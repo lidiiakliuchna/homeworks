67e5703ada476899f0fba6c2ea35d60fea582c98
Lidiia Kliuchna <lida.kluchnaya@gmail.com>
2023-03-31 23:43:03 +0100

    theory

--- hw4/shop.py
+++ hw4/shop.py
@@ -18,51 +18,53 @@ def greeting():
 
 greeting()
 answer=input("What would you like to choose?")
-if answer in items and answer!='sofa' and answer!='exit':
-    print(f'Here’s your {answer}!')
-elif answer=='sofa':
-    print(f'This one is too expensive! Do you have more money?')
-    answer1=input().lower()
-    if answer1 == 'yes':
-        def yes_money():
-            extra_money=int(input("How much more money do you have?"))
-            money1= money+extra_money
+def all_in_shop():
+    if answer in items and answer!='sofa' and answer!='exit':
+        print(f'Here’s your {answer}!')
+    elif answer=='sofa':
+        print(f'This one is too expensive! Do you have more money?')
+        answer1=input().lower()
+        if answer1 == 'yes':
+            def yes_money():
+                extra_money=int(input("How much more money do you have?"))
+                money1= money+extra_money
 
-            if money1>=shop["sofa"]:
-                print(f'Here’s your {answer}!')
+                if money1>=shop["sofa"]:
+                    print(f'Here’s your {answer}!')
 
-            else:
-                def choose_something():
-                    tries = 0
-                    while tries<=3:
-                        answer = input("Still too expensive. What another thing would you like to choose?")
-                        if answer in items and answer!='sofa' and answer!='exit':
-                            print(f'Here’s your {answer}!')
-                            return True
-                        else:
-                            print('It seems there is a problem. Try again')
-                            tries+=1
-                    print("Too many attempts. Good bye!")
-                choose_something()
-        yes_money()
+                else:
+                    def choose_something():
+                        tries = 0
+                        while tries<=3:
+                            answer = input("Still too expensive. What another thing would you like to choose?")
+                            if answer in items and answer!='sofa' and answer!='exit':
+                                print(f'Here’s your {answer}!')
+                                return True
+                            else:
+                                print('It seems there is a problem. Try again')
+                                tries+=1
+                        print("Too many attempts. Good bye!")
+                    choose_something()
+            yes_money()
+
+        else:
+            def no_money():
+                tries = 0
+                while tries<=3:
+
+                    answer = input("What another thing would you like to choose?")
+                    if answer in items and answer != 'sofa' and answer != 'exit':
+                        print(f'Here’s your {answer}!')
+                        return True
+                    else:
+                        print('It seems there is a problem. Try again')
+                        tries += 1
+                print("Too many attempts. Good bye!")
+            no_money()
+
+    elif answer == 'exit':
+        print("Good bye! See you again")
 
     else:
-        def no_money():
-            tries = 0
-            while tries<=3:
-
-                answer = input("What another thing would you like to choose?")
-                if answer in items and answer != 'sofa' and answer != 'exit':
-                    print(f'Here’s your {answer}!')
-                    return True
-                else:
-                    print('It seems there is a problem. Try again')
-                    tries += 1
-            print("Too many attempts. Good bye!")
-        no_money()
-
-elif answer == 'exit':
-    print("Good bye! See you again")
-
-else:
-    raise ValueError("Unknown Option Selected!")
+        raise ValueError("Unknown Option Selected!")
+all_in_shop()
