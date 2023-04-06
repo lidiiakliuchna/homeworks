80334fe18dda1cf477bd29bb494fa7e03a4843fa
Lidiia Kliuchna <lida.kluchnaya@gmail.com>
2023-04-06 11:23:35 +0100

    theory hw5

--- hw5/function.py
+++ hw5/function.py
@@ -1,4 +1,13 @@
 import collections
 s = "Write a function that takes in a sentence and returns the 3 most common letters."
-new_s=s.replace(" ", "")
-print(collections.Counter(new_s).most_common(1)[0])
+
+def most_common_letter():
+    new_letter = ""
+    new_s = s.replace(" ", "")
+    for i in range(3):
+        new_letter=collections.Counter(new_s).most_common(1)[0]
+        (letter, num) = new_letter
+
+        print(collections.Counter(new_s).most_common(1)[0])
+        new_s = new_s.replace(letter, "")
+most_common_letter()
