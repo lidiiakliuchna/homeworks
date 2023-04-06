93f2aa7a7c3112bb51627ad085e8e059b8748146
Lidiia Kliuchna <lida.kluchnaya@gmail.com>
2023-04-06 11:49:15 +0100

    theory hw5

--- hw5/function.py
+++ hw5/function.py
@@ -1,13 +1,22 @@
 import collections
-s = "Write a function that takes in a sentence and returns the 3 most common letters."
+s = input("Write your sentence here:")
 
 def most_common_letter():
-    new_letter = ""
-    new_s = s.replace(" ", "")
-    for i in range(3):
-        new_letter=collections.Counter(new_s).most_common(1)[0]
-        (letter, num) = new_letter
+    try:
+        count=0
+        new_letter = ""
+        new_s = s.replace(" ", "")
+        if len(new_s)<1:
+            print("There are no words.")
+        else:
+            while count<3:
+                new_letter=collections.Counter(new_s).most_common(1)[0]
+                (letter, num) = new_letter
 
-        print(collections.Counter(new_s).most_common(1)[0])
-        new_s = new_s.replace(letter, "")
+                print(collections.Counter(new_s).most_common(1)[0])
+                new_s = new_s.replace(letter, "")
+                count+=1
+    except:
+        if count<3:
+            print("Sentence has less than three unique letters")
 most_common_letter()
